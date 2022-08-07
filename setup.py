cb1685b4136ee69bf0a1d355a2691c15fe2c7680
Rajarshi Banerjee <rajarshiban13@gmail.com>
2022-08-07 21:29:59 +0530

    setup changed

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE/"readme.md").read_text()
 
 setup(
     name= "pytorchsummary",
-    version="1.0.1",
+    version="1.0.2",
     description="Summary of PyTorch Models just like `model.summary() in Keras",
+    long_description=README,
+    long_description_content_type='text/markdown',
     url="https://github.com/GSAUC3/pytorch-model-details",
     author="Rajarshi Banerjee",
     author_email="raju.banerjee.720@gmail.com",
